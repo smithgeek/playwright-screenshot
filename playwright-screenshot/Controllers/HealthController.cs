179348ffdb4e9df28e778fa62e8f689d5735092e
smithgeek <brent@smithgeek.com>
2022-12-28 16:26:06 -0600

    Add /health endpoint

---
+++ playwright-screenshot/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace playwright_screenshot.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class HealthController
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return new OkResult();
+		}
+	}
+}
